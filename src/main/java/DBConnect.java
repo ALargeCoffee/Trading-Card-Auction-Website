5b3ec3cfea108b29da155acee5681318c81179ad
kylearcilla <kylearcilla09@gmail.com>
2022-11-14 13:42:21 -0500

    sql file & .gitignore

--- src/main/java/DBConnect.java
+++ src/main/java/DBConnect.java
@@ -1,13 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-//Most of this will follow template project on Canvas
+// Most of this will follow template project on Canvas
 public class DBConnect {
-	
 	// Database connection object
 	public DBConnect() {}
 	
-	// Connects to database
+	// Connect to database
 	public Connection getConnection() {
 		// Get database connection URL
 		String connectionUrl = "jdbc:mysql://localhost:3306/tradingCardAuctionWebsite";
@@ -20,7 +19,7 @@ public class DBConnect {
 		}
 		// Use connection object to connect to DB
 		try {
-			con = DriverManager.getConnection(connectionUrl, "root", "g6%PXrw77$_");
+			con = DriverManager.getConnection(connectionUrl,"kylearcilla", "kyle13410");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -40,6 +39,7 @@ public class DBConnect {
 	public static void main(String[] args) {
 		DBConnect current = new DBConnect();
 		Connection con = current.getConnection();
+		
 		System.out.println(con);
 		current.closeConnection(con);
 	}
--- src/main/tradingCardAuctionWebsite.sql
+++ src/main/tradingCardAuctionWebsite.sql
@@ -1,2 +1,288 @@
-CREATE DATABASE IF NOT EXISTS 'tradingCardAuctionWebsite';
-USE 'tradingCardAuctionWebsite';
+CREATE DATABASE  IF NOT EXISTS `trading_card_auction_website` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `trading_card_auction_website`;
+-- MySQL dump 10.13  Distrib 8.0.26, for macos11 (x86_64)
+--
+-- Host: localhost    Database: trading_card_auction_website
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_staff_member`
+--
+
+DROP TABLE IF EXISTS `admin_staff_member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_staff_member` (
+  `admin_id` varchar(15) NOT NULL,
+  `admin_display_name` varchar(25) DEFAULT NULL,
+  `admin_password` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_staff_member`
+--
+
+LOCK TABLES `admin_staff_member` WRITE;
+/*!40000 ALTER TABLE `admin_staff_member` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_staff_member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buyer`
+--
+
+DROP TABLE IF EXISTS `buyer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buyer` (
+  `buyer_id` varchar(15) NOT NULL,
+  `wishlist` varchar(200) DEFAULT NULL,
+  `current_bids` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`buyer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buyer`
+--
+
+LOCK TABLES `buyer` WRITE;
+/*!40000 ALTER TABLE `buyer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buyer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buying`
+--
+
+DROP TABLE IF EXISTS `buying`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `buying` (
+  `bid_price` varchar(9) DEFAULT NULL,
+  `autobid_upper_limit` varchar(9) DEFAULT NULL,
+  `buyer_id` varchar(15) NOT NULL,
+  `auction_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`buyer_id`,`auction_id`),
+  KEY `auction_id` (`auction_id`),
+  CONSTRAINT `buying_ibfk_1` FOREIGN KEY (`buyer_id`) REFERENCES `buyer` (`buyer_id`),
+  CONSTRAINT `buying_ibfk_2` FOREIGN KEY (`auction_id`) REFERENCES `item` (`auction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buying`
+--
+
+LOCK TABLES `buying` WRITE;
+/*!40000 ALTER TABLE `buying` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buying` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_representative`
+--
+
+DROP TABLE IF EXISTS `customer_representative`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_representative` (
+  `representative_id` varchar(15) NOT NULL,
+  `cr_password` varchar(25) DEFAULT NULL,
+  `cr_display_name` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`representative_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_representative`
+--
+
+LOCK TABLES `customer_representative` WRITE;
+/*!40000 ALTER TABLE `customer_representative` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_representative` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `start_time` date DEFAULT NULL,
+  `end_time` date DEFAULT NULL,
+  `similar_items` varchar(200) DEFAULT NULL,
+  `bid_list` varchar(200) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `min_price` varchar(9) DEFAULT NULL,
+  `initial_price` varchar(9) DEFAULT NULL,
+  `increments` float DEFAULT NULL,
+  `auction_id` varchar(15) NOT NULL,
+  `categories` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`auction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manages`
+--
+
+DROP TABLE IF EXISTS `manages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manages` (
+  `representative_id` varchar(15) NOT NULL,
+  `admin_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`representative_id`,`admin_id`),
+  KEY `admin_id` (`admin_id`),
+  CONSTRAINT `manages_ibfk_1` FOREIGN KEY (`representative_id`) REFERENCES `customer_representative` (`representative_id`),
+  CONSTRAINT `manages_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin_staff_member` (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manages`
+--
+
+LOCK TABLES `manages` WRITE;
+/*!40000 ALTER TABLE `manages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moderates`
+--
+
+DROP TABLE IF EXISTS `moderates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `moderates` (
+  `representative_id` varchar(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  PRIMARY KEY (`representative_id`,`email`),
+  KEY `email` (`email`),
+  CONSTRAINT `moderates_ibfk_1` FOREIGN KEY (`representative_id`) REFERENCES `customer_representative` (`representative_id`),
+  CONSTRAINT `moderates_ibfk_2` FOREIGN KEY (`email`) REFERENCES `user` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moderates`
+--
+
+LOCK TABLES `moderates` WRITE;
+/*!40000 ALTER TABLE `moderates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moderates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seller`
+--
+
+DROP TABLE IF EXISTS `seller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `seller` (
+  `seller_id` varchar(15) NOT NULL,
+  `items_for_sale` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`seller_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seller`
+--
+
+LOCK TABLES `seller` WRITE;
+/*!40000 ALTER TABLE `seller` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `selling`
+--
+
+DROP TABLE IF EXISTS `selling`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `selling` (
+  `seller_id` varchar(15) NOT NULL,
+  `auction_id` varchar(15) NOT NULL,
+  PRIMARY KEY (`seller_id`,`auction_id`),
+  KEY `auction_id` (`auction_id`),
+  CONSTRAINT `selling_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`seller_id`),
+  CONSTRAINT `selling_ibfk_2` FOREIGN KEY (`auction_id`) REFERENCES `item` (`auction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling`
+--
+
+LOCK TABLES `selling` WRITE;
+/*!40000 ALTER TABLE `selling` DISABLE KEYS */;
+/*!40000 ALTER TABLE `selling` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `phone_number` int DEFAULT NULL,
+  `email` varchar(25) NOT NULL,
+  `user_display_name` varchar(25) DEFAULT NULL,
+  `user_password` varchar(25) DEFAULT NULL,
+  `auction_history` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2019380917,'user@gmail.com','test_user','1234',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-14 13:38:55
